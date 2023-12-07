e7518c272c0f742bf6d8e35d7a8536b1ebab7348
econisll <econisll@emeal.nttdata.com>
2023-12-07 00:09:44 -0500

    BENEFLEX-ADMINISTRATOR

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { IdpConfig } from '../config/auth.config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IdpService {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private userProfile!: any;
+
+  constructor(private readonly oauthService: OAuthService) {
+    this.configure();
+  }
+
+  private configure() {
+    this.oauthService.configure(IdpConfig);
+    this.oauthService.loadDiscoveryDocumentAndLogin().then((value) => {
+      if (value) {
+        this.oauthService.loadUserProfile().then((value) => {
+          this.userProfile = value;
+        });
+      }
+    });
+  }
+
+  logout() {
+    this.userProfile = null;
+    this.oauthService.logOut();
+  }
+
+  getUserProfile() {
+    return this.userProfile;
+  }
+}
